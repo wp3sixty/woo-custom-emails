394638f84e2992eee42ac9fcfd067f623f5babb9
Utkarsh Patel <itismeutkarsh@gmail.com>
2026-02-10 18:14:13 +0530

    Bump tested versions (WP 6.9, WC 10.5), add Requires Plugins header, and update wp-env to WP 6.9

--- woo-custom-emails.php
+++ woo-custom-emails.php
@@ -8,10 +8,11 @@
  * Author: wp3sixty
  * Author URI: http://wp3sixty.com
  * Requires at least: 6.0
- * Tested up to: 6.7
+ * Tested up to: 6.9
  * Requires PHP: 7.4
+ * Requires Plugins: woocommerce
  * WC requires at least: 8.0
- * WC tested up to: 9.5
+ * WC tested up to: 10.5
  *
  * Text Domain: woo-custom-emails
  *
